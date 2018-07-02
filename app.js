1296bf9a8a226b0534bed1eae76a3e4f34ffff71
DanXar <dan001@ucsd.edu>
2018-07-02 10:54:01 -0700

    node.js capability added

---
+++ app.js
@@ -0,0 +1,38 @@
+var url = require('url');
+var fs = require('fs');
+
+function render(path, response) {
+	fs.readFile(path, null, function(error, data) {
+		if(error){
+			response.writeHead(404);
+			response.write('File not found');
+		}
+		else{
+			response.write(data);
+		}
+		response.end();
+	});
+}
+
+module.exports = {
+	handleRequest: function(request, response){
+		response.writeHead(200, {'Content-Type': 'text/html'});
+		
+		var path = url.parse(request.url).pathname;
+		switch(path){
+			case '/':
+				render('./home.html', response);
+				break;
+			/*case '/process':
+				render('./process.html', response);
+				break;
+			case '/gpsScript.js':
+				render('./gpsScript.js', response);
+				break;*/
+			default:
+				response.writeHead(404);
+				response.write('Route not defined');
+				response.end();
+		}
+	}
+};
