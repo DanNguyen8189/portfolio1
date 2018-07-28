5e7951c69ddfa2fbe420e447ee5dd64fe394ba83
DanXar <dan001@ucsd.edu>
2018-07-28 15:35:54 -0700

    small banner change before diving into router transitions

--- src/Navigation-component.vue
+++ src/Navigation-component.vue
@@ -11,6 +11,7 @@
     font-size: 17px;
     font-family: 'Actor', sans-serif;
     color: #ffffffc2;
+    background-color: rgba(0,0,0,0.05);
     transition: color linear 0.15s;
 }
 @media screen and (min-width: 860px) {
@@ -19,36 +20,10 @@
         box-shadow:0px 1px 1px rgba(255, 255, 255, 0.30);
         border-radius:3px;
     }
-    .drop-down-menu{
-        display: none;
-    }
-    .drop-down-symbol{
-        display: none;
-    }
 }
 @media screen and (max-width: 860px) {
     #bar-menu{
         display: none
-    }
-
-    .drop-down-menu{
-        display: inline-block;
-    }
-
-    .drop-down-symbol{
-        display: inline-block;
-    }
-    
-    /*.drop-down-symbol:hover, .drop-down-symbol.active {
-        text-decoration:none;
-        color:gray;
-    }*/
-
-    .fade-enter-active, .fade-leave-active {
-        transition: opacity .5s;
-    }
-    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
-        opacity: 0;
     }
 }
 </style>
