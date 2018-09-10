235d1d34c730cee6b4a0998abc2ed9806016520b
DanXar <dan001@ucsd.edu>
2018-09-10 16:48:16 -0700

    Styling edits for contact form

--- src/Contact-page.vue
+++ src/Contact-page.vue
@@ -16,28 +16,33 @@
 
         <!--Contact form-->
         <form class="vue-form" @submit.prevent="submit">
-
+            <legend>Feel free to drop a line</legend>
             <div class="error-message">
+                <!--Conditional rendering-->
                 <p v-show="!email.valid">Oh, please enter a valid email address.</p>
             </div>
 
             <fieldset>
             <div>
                 <label class="label" for="name">Name</label>
-                <input type="text" name="name" id="name" required="" v-model="name">
+                <input type="text" name="name" id="name" required="" 
+                    v-model="name"
+                    placeholder="Name">
             </div>
             <div>
                 <label class="label" for="email">Email</label>
                 <input type="email" name="email" id="email" required=""
                     :class="{ email , error: !email.valid }"
-                    v-model="email.value">
+                    v-model="email.value"
+                    placeholder="example@domain.com">
             </div>
 
             <div>
-                <label class="label" for="textarea">Message with Counter</label>
+                <label class="label" for="textarea">Message</label>
                 <textarea class="message" name="textarea" id="textarea" required="" 
-                        v-model="message.text" 
-                        :maxlength="message.maxlength"></textarea>
+                    v-model="message.text" 
+                    placeholder="Enter message"
+                    :maxlength="message.maxlength"></textarea>
                 <span class="counter">{{ message.text.length }} / {{ message.maxlength }}</span>
             </div>
             <div>
@@ -71,13 +76,13 @@ export default{
     data() {
         return {
             //default data
-            name: "John Doe",
+            name: "",
             email: {
-                value: "jo@hnd.oe",
+                value: "",
                 valid: true
             },
             message: {
-                text: `Hello`,
+                text: ``,
                 maxlength: 255
             },
             submitted: false
@@ -99,7 +104,7 @@ export default{
             return emailRegExp.test(value);
         },
 
-        },
+    },
     //continously watches the email field to confirm validity
     watch: {
         "email.value": function(value) {
@@ -268,7 +273,7 @@ p{
     }
 }
 
-*,
+/**,
 *::after,
 *::before {
     box-sizing: border-box;
@@ -316,24 +321,25 @@ header h1 {
     text-align: center;
     font-size: 2.4rem;
     font-weight: 300;
-}
+}*/
 
 .vue-form {
     font-size: 16px;
     width: 500px;
     padding: 15px 30px;
     border-radius: 4px;
-    margin: 50px auto;
+    margin: 25px auto;
     width: 500px;
-    background-color: #fff;
+    background-color: rgb(127, 182, 148);
     box-shadow: 0 4px 6px 0 rgba(0, 0, 0, 0.3);
 }
 .vue-form fieldset {
-    margin: 24px 0 0 0;
+    margin: 0 0 0 0;
 }
 .vue-form legend {
     padding-bottom: 10px;
     border-bottom: 1px solid #ecf0f1;
+    font-family: 'Architects Daughter', sans-serif;
 }
 .vue-form div {
     position: relative;
@@ -375,7 +381,6 @@ header h1 {
     border-color: #2c3e50;
     box-shadow: 0 0 5px rgba(44, 151, 222, 0.2);
 }
-
 .vue-form .vue-form-list {
     margin-top: 16px;
 }
@@ -389,77 +394,6 @@ header h1 {
     position: relative;
     user-select: none;
     margin: 0 26px 16px 0;
-    float: left;
-}
-.vue-form input[type="radio"],
-.vue-form input[type="checkbox"] {
-    position: absolute;
-    left: 0;
-    top: 50%;
-    transform: translateY(-50%);
-    margin: 0;
-    padding: 0;
-    opacity: 0;
-    z-index: 2;
-}
-.vue-form input[type="radio"] + label,
-.vue-form input[type="checkbox"] + label {
-    padding-left: 24px;
-}
-.vue-form input[type="radio"] + label::before,
-.vue-form input[type="radio"] + label::after,
-.vue-form input[type="checkbox"] + label::before,
-.vue-form input[type="checkbox"] + label::after {
-    content: "";
-    display: block;
-    position: absolute;
-    left: 0;
-    top: 50%;
-    margin-top: -8px;
-    width: 16px;
-    height: 16px;
-}
-.vue-form input[type="radio"] + label::before,
-.vue-form input[type="checkbox"] + label::before {
-    border: 1px solid #cfd9db;
-    background: #ffffff;
-    box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.08);
-}
-.vue-form input[type="radio"] + label::before,
-.vue-form input[type="radio"] + label::after {
-    border-radius: 50%;
-}
-.vue-form input[type="checkbox"] + label::before,
-.vue-form input[type="checkbox"] + label::after {
-    border-radius: 0.25em;
-}
-.vue-form input[type="radio"] + label::after,
-.vue-form input[type="checkbox"] + label::after {
-    background-color: #2c3e50;
-    background-position: center center;
-    background-repeat: no-repeat;
-    box-shadow: 0 0 5px rgba(44, 151, 222, 0.4);
-    display: none;
-}
-.vue-form input[type="radio"] + label::after {
-    background-image: url("data:image/svg+xml;charset=utf-8,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22utf-8%22%3F%3E%0D%0A%3C%21DOCTYPE%20svg%20PUBLIC%20%22-%2F%2FW3C%2F%2FDTD%20SVG%201.1%2F%2FEN%22%20%22http%3A%2F%2Fwww.w3.org%2FGraphics%2FSVG%2F1.1%2FDTD%2Fsvg11.dtd%22%3E%0D%0A%3Csvg%20version%3D%221.1%22%20id%3D%22Layer_1%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20x%3D%220px%22%20y%3D%220px%22%0D%0A%09%20width%3D%2216px%22%20height%3D%2216px%22%20viewBox%3D%220%200%2016%2016%22%20enable-background%3D%22new%200%200%2016%2016%22%20xml%3Aspace%3D%22preserve%22%3E%0D%0A%3Ccircle%20fill%3D%22%23FFFFFF%22%20cx%3D%228%22%20cy%3D%228%22%20r%3D%223%22%2F%3E%0D%0A%3C%2Fsvg%3E");
-}
-.vue-form input[type="checkbox"] + label::after {
-    background-image: url("data:image/svg+xml;charset=utf-8,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22utf-8%22%3F%3E%0D%0A%3C%21--%20Generator%3A%20Adobe%20Illustrator%2018.1.1%2C%20SVG%20Export%20Plug-In%20.%20SVG%20Version%3A%206.00%20Build%200%29%20%20--%3E%0D%0A%3C%21DOCTYPE%20svg%20PUBLIC%20%22-%2F%2FW3C%2F%2FDTD%20SVG%201.1%2F%2FEN%22%20%22http%3A%2F%2Fwww.w3.org%2FGraphics%2FSVG%2F1.1%2FDTD%2Fsvg11.dtd%22%3E%0D%0A%3Csvg%20version%3D%221.1%22%20id%3D%22Layer_1%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20xmlns%3Axlink%3D%22http%3A%2F%2Fwww.w3.org%2F1999%2Fxlink%22%20x%3D%220px%22%20y%3D%220px%22%0D%0A%09%20width%3D%2216px%22%20height%3D%2216px%22%20viewBox%3D%220%200%2016%2016%22%20enable-background%3D%22new%200%200%2016%2016%22%20xml%3Aspace%3D%22preserve%22%3E%0D%0A%3Cpolyline%20fill%3D%22none%22%20stroke%3D%22%23FFFFFF%22%20stroke-width%3D%222%22%20stroke-linecap%3D%22square%22%20stroke-miterlimit%3D%2210%22%20points%3D%225%2C8%207%2C10%2011%2C6%20%22%2F%3E%0D%0A%3C%2Fsvg%3E");
-}
-.vue-form input[type="radio"]:focus + label::before,
-.vue-form input[type="checkbox"]:focus + label::before {
-    box-shadow: 0 0 5px rgba(44, 151, 222, 0.6);
-}
-.vue-form input[type="radio"]:checked + label::after,
-.vue-form input[type="checkbox"]:checked + label::after {
-    display: block;
-}
-.vue-form input[type="radio"]:checked + label::before,
-.vue-form input[type="radio"]:checked + label::after,
-.vue-form input[type="checkbox"]:checked + label::before,
-.vue-form input[type="checkbox"]:checked + label::after {
-    animation: cd-bounce 0.3s;
 }
 .vue-form textarea {
     min-height: 120px;
@@ -477,7 +411,6 @@ header h1 {
     cursor: pointer;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    appearance: none;
 }
 .no-touch .vue-form input[type="submit"]:hover {
     background: #42a2e1;
@@ -494,7 +427,7 @@ header h1 {
     margin: 0px;
 }
 .vue-form .error-message p {
-    background: #e94b35;
+    background: #a03527;
     color: #ffffff;
     font-size: 1.4rem;
     text-align: center;
@@ -504,7 +437,7 @@ header h1 {
     padding: 16px;
 }
 .vue-form .error {
-    border-color: #e94b35 !important;
+    border-color: #a7392a !important;
 }
 .vue-form .counter {
     background-color: #ecf0f1;
